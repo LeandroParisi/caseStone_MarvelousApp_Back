3b129bc1ed50f311323f8349de4072bbb1457fa9
LeandroParisi <lepcbelisario@gmail.com>
2021-04-10 18:14:11 -0300

    setup: imports and sets routers on index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const errorHandler = require('./src/middlewares/errorHandler/errorHandler');
+const { UsersRouter, CharactersRouter, ComicsRouter } = require('./src/routes');
+
 require('dotenv').config();
 
-const PORT = process.env.PORT || customPort;
+const PORT = process.env.PORT
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/user', UsersRouter)
+app.use('/character', CharactersRouter)
+app.use('/controller', ComicsRouter)
 
 app.use(errorHandler);
 
