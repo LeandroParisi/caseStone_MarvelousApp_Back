c9f7f1b1bc119a25863d6fd5f6d8d42de73ff61e
LeandroParisi <lepcbelisario@gmail.com>
2021-04-10 17:42:17 -0300

    setup: creates UsersCharacters seeder

---
+++ seeders/20210410203932-populateUsersCharacters.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('UsersCharacters', 
+      [{
+        userId: 1,
+        characterId: 1,
+      }],
+      [{
+        userId: 1,
+        characterId: 2,
+      }],
+      [{
+        userId: 1,
+        characterId: 3,
+      }],
+      [{
+        userId: 2,
+        characterId: 4,
+      }],
+      [{
+        userId: 2,
+        characterId: 5,
+      }],
+      [{
+        userId: 2,
+        characterId: 6,
+      }],
+    )},
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete('UsersCharacters', null, {});
+  }
+};
