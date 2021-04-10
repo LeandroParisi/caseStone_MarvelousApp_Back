c3c84e3f970c762625f5805eeab263e7b70a42a4
LeandroParisi <lepcbelisario@gmail.com>
2021-04-10 19:26:37 -0300

    feat: implements login endpoint

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,22 +1,26 @@
 const { Users } = require('../models');
-const { status, messages } = require('../libs');
+const { status, resMessages } = require('../libs');
 const { FireError } = require('../middlewares/errorHandler/utils');
-const { jwtConfig, createJWTPayload, jwtSign } = require('../authentication/jwtConfig');
+const { generateToken } = require('../authentication/jwtConfig');
 
 const createUser = async (req, res) => {
-  res.status(200).json({ message: 'createUser' });
+  res.status(status.ok).json({ message: 'createUser' });
 };
 
 const login = async (req, res) => {
-  res.status(200).json({ message: 'login' });
+  const { email, password } = req.body;
+
+  const token = generateToken({ email, password });
+
+  res.status(status.ok).json({ message: resMessages.loginOK, token });
 };
 
 const updateUser = async (req, res) => {
-  res.status(200).json({ message: 'updateUser' });
+  res.status(status.ok).json({ message: 'updateUser' });
 };
 
 const deleteUser = async (req, res) => {
-  res.status(200).json({ message: 'deleteUser' });
+  res.status(status.ok).json({ message: 'deleteUser' });
 };
 
 module.exports = {
