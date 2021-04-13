b15c8bab2b4395763a0bfbcc50138df45ddbd52d
LeandroParisi <lepcbelisario@gmail.com>
2021-04-13 20:19:16 -0300

    deploy: deploying on heroku

--- config/config.js
+++ config/config.js
@@ -4,9 +4,15 @@ module.exports = {
   development: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'marvelous_development',
+    database: process.env.DATABASE_URL,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: process.env.MYSQL_USER,
