6f04558a49e76574aa445b160fe5a419de6b2d64
LeandroParisi <lepcbelisario@gmail.com>
2021-04-13 21:50:03 -0300

    setup: sets all sequelize config to PG

--- config/config.js
+++ config/config.js
@@ -2,18 +2,32 @@ require('dotenv/config');
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: 'marvelous_development',
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.DATABASE,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: 'marvelous_test',
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.DATABASE,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: process.env.PG_USER,
